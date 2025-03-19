d6a251cea64f2f5d5f6291b5a14b3eee0f080821
Huzwif2 <syedhu@sheridancollege.ca>
2025-03-19 09:21:58 -0400

    works but for some reason if wash.png exists in assets folder it segfaults for no reason (core dumped)

--- main.cpp
+++ main.cpp
@@ -1,76 +1,85 @@
-#include <iostream>
-#include <raylib.h>
-#define NUM_FRAMES 3
+ #include <iostream>
+ #include <raylib.h>
+ #define NUM_FRAMES 3
 
-const int screenWidth = 1024;
-const int screenHeight = 768;
+ const int screenWidth = 1024;
+ const int screenHeight = 768;
 
-Texture2D texBackground;
-Texture2D texToast;
-Texture2D texFridge;
-Texture2D texWash;
 
-void GameStartup() {
-	//Image imgBackground = LoadImage("assets/background.png");
-	//texBackground = LoadTextureFromImage(imgBackground);
-	//UnloadImage(imgBackground);
+ //Image imgBackground = LoadImage("assets/background.png");
+ //texBackground = LoadTextureFromImage(imgBackground);
+ //UnloadImage(imgBackground);
 
-}
 
-void GameUpdate() {
 
-	if (IsKeyDown(KEY_SPACE)) {
-		Image imgToast = LoadImage("assets/toast.png");
-		texToast = LoadTextureFromImage(imgToast);
-		    DrawTexture(texToast, screenWidth/2 - texToast.width/2, screenHeight/2 - texToast.height/2 - 40, WHITE);
-		    UnloadImage(imgToast);
-		}
 
-	if (IsKeyDown(KEY_K)) {
-		Image imgFridge = LoadImage("assets/fridge.jpg");
-		texFridge = LoadTextureFromImage(imgFridge);
-		    DrawTexture(texFridge, screenWidth/2 - texFridge.width/2, screenHeight/2 - texFridge.height/2 - 40, WHITE);
-		    UnloadImage(imgFridge);
-		}
+ int main() {
 
-	if (IsKeyDown(KEY_W)) {
-		Image imgWash = LoadImage("assets/wash.png");
-		texWash = LoadTextureFromImage(imgWash);
-		    DrawTexture(texWash, screenWidth/4 - texWash.width/4, screenHeight/4 - texWash.height/4 - 40, WHITE);
-		    UnloadImage(imgWash);
-		}
-}
+         std::cout << "hello world!\n";
+         InitWindow (screenWidth, screenHeight, "Kitchen RNG");
 
-void GameRender() {
-	//DrawTexture (texBackground, 0, 0, RAYWHITE);
-}
+         SetTargetFPS(60);
 
-void GameShutdown() {
-}
+         Image toast = LoadImage("assets/toast.png");
+         Texture2D texToast = LoadTextureFromImage(toast);
+         UnloadImage(toast);
 
-int main () {
-	std::cout << "hello world!\n";
-	InitWindow (screenWidth, screenHeight, "Kitchen RNG");
+         Image wash = LoadImage("assets/wash.png");
+         Texture2D texWash = LoadTextureFromImage(wash);
+         UnloadImage(wash);
 
-	SetTargetFPS(60);
+         Image fridge = LoadImage("assets/fridge.jpg");
+         Texture2D texFridge = LoadTextureFromImage(fridge);
+         UnloadImage(wash);
 
-	GameStartup();
 
-	while (!WindowShouldClose()) {
-		
-		GameUpdate();
+ //      if (IsKeyDown(KEY_SPACE)) {
+ //              image imgToast = LoadImage("assets/toast.png");
+ //              texToast = LoadTextureFromImage(imgToast);
+ //                  DrawTexture(texToast, screenWidth/2 - texToast.width/2, screenHeight/2 - texToast.height/2 - 40, WHITE)    ;
+ //                  UnloadImage(imgToast);
+ //              }
+ //
+ //      if (iskeyDown(KEY_K)) {
+ //              image imgFridge = LoadImage("assets/fridge.jpg");
+ //              texFridge = LoadTextureFromImage(imgFridge);
+ //                  DrawTexture(texFridge, screenWidth/2 - texFridge.width/2, screenHeight/2 - texFridge.height/2 - 40, WHI    TE);
+ //                  UnloadImage(imgFridge);
+ //              }
+ //
+ //      if (iskeyDown(KEY_W)) {
+ //              image imgWash = LoadImage("assets/wash.png");
+ //              texWash = LoadTextureFromImage(imgWash);
+ //                  DrawTexture(texWash, screenWidth/4 - texWash.width/4, screenHeight/4 - texWash.height/4 - 40, WHITE);
+ //                  UnloadImage(imgWash);
 
-		BeginDrawing();
-		ClearBackground(GREEN);
+         int num = 0;
 
-		GameRender();
+         while (!WindowShouldClose()) {
 
-		EndDrawing();
-	}
 
-	GameShutdown();
-	CloseWindow();
+                 if(IsKeyPressed(KEY_SPACE)) {
+                         num = GetRandomValue(0,2); // Three possible outcomes
+                 }
 
-	return 0;
-}
+                 BeginDrawing();
+                 ClearBackground(GREEN);
+
+                 if (num == 0) {
+                         DrawTexture(texToast, screenWidth/2 - texToast.width/2, screenHeight/2 - texToast.height/2 - 40, WHITE);
+                 }
+                 else if (num == 1) {
+                         DrawTexture(texWash, screenWidth/4 - texWash.width/4, screenHeight/4 - texWash.height/4 - 40, WHITE    );
+                 }
+                 else if (num == 2) {
+                         DrawTexture(texFridge, screenWidth/2 - texFridge.width/2, screenHeight/2 - texFridge.height/2 - 40,     WHITE);
+                 }
+
+                 EndDrawing();
+         }
+
+         CloseWindow();
+
+         return 0;
+ }
 
