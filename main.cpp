560b9023a7a365238c60cf55f0adc3c07f5ee19d
Huzwif2 <syedhu@sheridancollege.ca>
2025-04-13 20:22:10 -0400

    trying DrawTexturePro

--- main.cpp
+++ main.cpp
@@ -29,38 +29,47 @@
          Texture2D texFridge = LoadTextureFromImage(fridge);
          UnloadImage(fridge);
 
-
-
-
          int num = 0;
-
          while (!WindowShouldClose()) {
-
-
                  if(IsKeyPressed(KEY_SPACE)) {
                          num = GetRandomValue(0,2); // Three possible outcomes
                  }
 
                  BeginDrawing();
-		 DrawTexture(texBackground, 0, 0, WHITE);
+				 DrawTexture(texBackground, 0, 0, WHITE);
                  ClearBackground(GREEN);
-
-                 if (num == 0) {
-                         DrawTexture(texToast, screenWidth/2 - texToast.width/2, screenHeight/2 - texToast.height/2 - 40, WHITE);
+				
+				 if (num == 0) {
+						 Rectangle washRect = {0, 500, 400, 400};
+					 // DrawTexturePro(Texture2D , Rectangle source, Rectangle dest, Vector2 origin, float rotation, Color tint); 
+						DrawTexturePro(
+										texToast,
+										washRect,  
+										(Rectangle) {s, 
+										0,
+									   	WHITE
+										); 
                  }
+
                  else if (num == 1) {
-                         DrawTexture(texWash, screenWidth/4 - texWash.width/4, screenHeight/4 - texWash.height/4 - 40, WHITE    );
+                         DrawTexture(
+							 texWash,
+							 screenWidth/4 - texWash.width/4,
+							 screenHeight/4 - texWash.height/4 - 40,
+							 WHITE
+							 );
                  }
                  else if (num == 2) {
-                         DrawTexture(texFridge, screenWidth/2 - texFridge.width/2, screenHeight/2 - texFridge.height/2 - 40,     WHITE);
+                         DrawTexture(
+									texFridge,
+									screenWidth/2 - texFridge.width/2,
+									screenHeight/2 - texFridge.height/2 - 40,
+									WHITE
+									);
                  }
-
-
                  EndDrawing();
          }
 
          CloseWindow();
-
          return 0;
  }
-
